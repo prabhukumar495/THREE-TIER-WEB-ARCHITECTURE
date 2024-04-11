e6952d59bc1ebcdbc60a54b009ba1ce0339bfe62
prabhukumar495 <109064542+prabhukumar495@users.noreply.github.com>
2024-04-11 19:43:46 +0530

    Add files via upload

---
+++ application-code/app-tier/DbConfig.js
@@ -0,0 +1,6 @@
+module.exports = Object.freeze({
+    DB_HOST : 'threet-wap-db-ins.ctqeqogwa8ao.us-east-1.rds.amazonaws.com',
+    DB_USER : 'admin',
+    DB_PWD : 'admin1234',
+    DB_DATABASE : 'webappdb'
+});
